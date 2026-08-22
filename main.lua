8493e3ef733e178f15d1b9b1a070c56046f71d01
mario luiz <marioluizsan@gmail.com>
2026-08-22 18:41:50 -0300

    .

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 -- atribuindo o lua rocks
 
-local version = _VERSION:sub(5,8)
-package.cpath = "./lua_modules/lib64/lua/"..version.."?.so;./deps/?.so;" .. package.cpath
+local v = _VERSION:sub(5,8)
+package.cpath = "./lua_modules/lib64/lua/"..v.."?.so;./deps/?.so;" .. package.cpath
 
 local bcrypt = require('bcrypt')
 
