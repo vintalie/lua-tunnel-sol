710408458419bb573338136f6927a57e91763636
mario luiz <marioluizsan@gmail.com>
2026-08-23 10:36:36 -0300

    .

--- main.lua
+++ main.lua
@@ -2,13 +2,7 @@
 
 local version = _VERSION:sub(5,8)
 -- importante, referenciar os pacotes tais como os incas e os astecas
-lua_package_path "/etc/nginx/lua-tunnel-sol/lua_modules/?.lua;;";
-package.cpath = "/etc/nginx/lua-tunnel-sol/lua_modules/lib64/lua/"..version.."/?.so;./deps/?.so;" .. package.cpath
 
-local bcrypt = require('bcrypt')
+package.cpath = "/etc/nginx/lua-tunnel-sol/lua_modules/lib/lua/"..version.."/?.so;./deps/?.so;" .. package.cpath
 
-local password = "123"
-local hashed = bcrypt.digest(password, 10)
---testando o lua rocks com o openresty
-ngx.header.content_type = "text/plain"
-ngx.say("Hello World \n" .. hashed)
+ngx.say("Hello World \n")
